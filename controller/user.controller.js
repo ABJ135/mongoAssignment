a4da9dd59401e127ace32908f7c9ddcd059720c6
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2024-11-26 06:29:54 +0500

    error resolved

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,74 +1,75 @@
-const user = require('../models/user.models')
+const user = require("../models/user.models");
 
-const createUser = async(req,res)=>{
-    const data = req.body
-    const object = await user.create(data)
-    res.json({"message":"Object created Successfully",object})
-}
+const createUser = async (req, res) => {
+  const data = req.body;
+  const object = await user.create(data);
+  res.json({ message: "Object created Successfully", object });
+};
 
-const getAllUsers = async(req,res)=>{
-const object = await user.find({})
-res.json(object)
-}
+const getAllUsers = async (req, res) => {
+  const object = await user.find({});
+  res.json(object);
+};
 
-const getUserById = async(req,res)=>{
-    try{
-        const id = req.params.id        
-        const object = await user.findById(id).populate('bookId')
-        res.json(object)
-    }catch(error){
-        console.log(error)
-        res.send("Error finding user")
-    }
-}
+const getUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const object = await user.findById(id).populate("bookId");
+    res.json(object);
+  } catch (error) {
+    console.log(error);
+    res.send("Error finding user");
+  }
+};
 
-const updateUser = async(req,res)=>{
-    const id = req.params.id
-    const data = req.body
-    const object =await user.findByIdAndUpdate(id,data)
+const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+  const object = await user.findByIdAndUpdate(id, data);
 
-    res.json(object)
-}
+  res.json(object);
+};
 
-const deleteUser = async (req,res)=>{
-    const id = req.params.id
-    await user.deleteOne(id);
-}
+const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  await user.deleteOne(id);
+};
 
-const borrow = async (req,res)=>{
-    const userId = req.params.Id
-    const bookId = req.params.bookId
-    // res.json(userId)
-   
-        
-        // const obj = await user.findById(userId)
-        // res.json(obj)
+const borrow = async (req, res) => {
+  const userId = req.params.Id;
+  const bookId = req.params.bookId;
+  
+    try
+  {const obj = await user.findById(userId)
 
-        // if(!obj){
-        //     return res.status(400).json("user not found")
-        // }
 
-        // if(obj.borrowedBook.length === 3){
-        //   return  res.status(400).json("user cannot borrow more books")
-        // }
+  if(!obj){
+      return res.status(400).json("user not found")
+  }
 
-        // if (obj.borrowedBook.includes(bookId)) {
-        //     return res.status(400).json("Book already borrowed");
-        // }
+  if(obj.borrowedBook.length === 3){
+    return  res.status(400).json("user cannot borrow more books")
+  }
 
-        // obj.borrowedBook.push(bookId);
+  if (obj.borrowedBook.includes(bookId)) {
+      return res.status(400).json("Book already borrowed");
+  }
 
-        // await obj.save();
+  obj.borrowedBook.push(bookId);
 
-        // res.status(200).json("Book borrowed successfully")
-    
-}
+  await obj.save();
 
-module.exports  = {
-    createUser,
-    getAllUsers,
-    getUserById,
-    updateUser,
-    deleteUser,
-    borrow
-}
+  res.status(200).json("Book borrowed successfully")}
+  catch(error){
+    res.status(500).json(error)
+  }
+};
+
+module.exports = {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  borrow,
+};
