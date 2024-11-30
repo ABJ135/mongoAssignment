1f5651a179586bbbd02d166b2421a496a34bd698
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2024-11-30 05:15:29 +0500

    hashing complete

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,7 +1,12 @@
 const user = require("../models/user.models");
+const bcrypt = require("bcrypt");
 
 const createUser = async (req, res) => {
   const data = req.body;
+  let pass = data.password;
+
+  pass =await bcrypt.hash(pass, 10);
+  data.password = pass;
   const object = await user.create(data);
   res.json({ message: "Object created Successfully", object });
 };
@@ -38,91 +43,85 @@ const deleteUser = async (req, res) => {
 const borrow = async (req, res) => {
   const userId = req.params.Id;
   const bookId = req.params.bookId;
-  
-    try
-  {const obj = await user.findById(userId)
 
+  try {
+    const obj = await user.findById(userId);
 
-  if(!obj){
-      return res.status(400).json("user not found")
-  }
+    if (!obj) {
+      return res.status(400).json("user not found");
+    }
 
-  if(obj.borrowedBook.length === 3){
-    return  res.status(400).json("user cannot borrow more books")
-  }
+    if (obj.borrowedBook.length === 3) {
+      return res.status(400).json("user cannot borrow more books");
+    }
 
-  if (obj.borrowedBook.includes(bookId)) {
+    if (obj.borrowedBook.includes(bookId)) {
       return res.status(400).json("Book already borrowed");
-  }
+    }
 
-  obj.borrowedBook.push(bookId);
+    obj.borrowedBook.push(bookId);
 
-  await obj.save();
+    await obj.save();
 
-  res.status(200).json("Book borrowed successfully")}
-  catch(error){
-    res.status(500).json(error)
+    res.status(200).json("Book borrowed successfully");
+  } catch (error) {
+    res.status(500).json(error);
   }
 };
 
-const returnBook = async(req,res) => {
-    const userId = req.params.id
-    const bookId = req.params.bookId
-    console.log(userId,"userid",bookId,"bookId")
+const returnBook = async (req, res) => {
+  const userId = req.params.id;
+  const bookId = req.params.bookId;
+  console.log(userId, "userid", bookId, "bookId");
 
-    try{
-        const obj = await user.findById(userId)
+  try {
+    const obj = await user.findById(userId);
 
-        if(!obj){
-            res.status(400).json("user not found")
-        }
-
-        if(obj.borrowedBook.length === 0){
-            res.status(400).json("user has not borrowed any book")
-        }
-
-        const index = obj.borrowedBook.findIndex(book => book.toString() === bookId);
-        obj.borrowedBook.splice(index,1)
-
-        await obj.save();
-        res.status(200).json("Book removed successfully")
+    if (!obj) {
+      res.status(400).json("user not found");
     }
-    catch(error)
-    {
-        res.status(500).json(error,"can not return book")
+
+    if (obj.borrowedBook.length === 0) {
+      res.status(400).json("user has not borrowed any book");
     }
-}
 
-const borrowedBooks = async(req,res) => {
-    try {
-        const id = req.params.id;
-        const object = await user.findById(id).populate("borrowedBook");
-        res.json(object);
-      } catch (error) {
-        console.log(error);
-        res.send("Error finding user");
-      }
-}
+    const index = obj.borrowedBook.findIndex(
+      (book) => book.toString() === bookId
+    );
+    obj.borrowedBook.splice(index, 1);
 
-const login = async(req,res)=>{
-  try
-  {
-    const data = req.body
-    const email = await user.findOne({email:req.body.email})
-
-    if(email && email.password === req.body.password){
-      res.status(400).json("Login Successfull")
-    }     
-      else{
-        res.status(500).json("wrong Credentials")
-      }
-    
-  }catch(error)
-  {
-    res.status(500).json("Error")
+    await obj.save();
+    res.status(200).json("Book removed successfully");
+  } catch (error) {
+    res.status(500).json(error, "can not return book");
   }
-  
-}
+};
+
+const borrowedBooks = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const object = await user.findById(id).populate("borrowedBook");
+    res.json(object);
+  } catch (error) {
+    console.log(error);
+    res.send("Error finding user");
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const data = req.body;
+    const email = await user.findOne({ email: req.body.email });
+
+    if (email && email.password === req.body.password) {
+      res.status(400).json("Login Successfull");
+    } else {
+      res.status(500).json("wrong Credentials");
+    }
+  } catch (error) {
+    res.status(500).json("Error");
+  }
+};
 
 module.exports = {
   createUser,
@@ -133,5 +132,5 @@ module.exports = {
   borrow,
   returnBook,
   borrowedBooks,
-  login
+  login,
 };
